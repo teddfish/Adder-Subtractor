ac86af34b16e2ff0a7bc96220355d5251fcdb047
Shwet <shwetaketu1@gmail.com>
2021-04-25 18:09:28 +0530

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers");
+            int i = Int32.Parse(Console.ReadLine());
+            int j = Int32.Parse(Console.ReadLine());
+
+            int ans = Add(i, j);
+            int ans2 = Subtract(i, j);
+
+            Console.WriteLine("result of addition is " + ans);
+            Console.WriteLine("result of subtraction is " + ans2); 
+            Console.Read();
+            
+        }
+
+        public static int Add(int i, int j)
+        {
+            int add = i ^ j;
+            int carry = i & j;
+
+            while (carry != 0) 
+            {
+                int newcarry = carry << 1;
+
+                int newsum = add ^ newcarry;
+                carry = add & newcarry;
+                //Console.WriteLine(carry);
+
+                add = newsum;
+            }
+            return add;
+        }
+
+        public static int Subtract(int i, int j) 
+        {
+            int add = ~j ^ 1;
+            int carry = ~j & 1;
+
+            while (carry != 0)
+            {
+                int newcarry = carry << 1;
+
+                int newsum = add ^ newcarry;
+                carry = add & newcarry;
+                //Console.WriteLine(carry);
+
+                add = newsum;
+            }
+
+            int add2 = i ^ add;
+            int carry2 = i & add;
+
+            while (carry2 != 0)
+            {
+                int newcarry2 = carry2 << 1;
+
+                int newsum2 = add2 ^ newcarry2;
+                carry2 = add2 & newcarry2;
+                //Console.WriteLine(carry2);
+
+                add2 = newsum2;
+            }
+            return add2;
+
+        }
+    }
+
+        
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class City
+    {
+        string s;
+        public float lat, lon;
+        public City(string s1, float lat1, float lon1)
+        {
+            s = s1;
+            lat = lat1;
+            lon = lon1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            City[] c = { new City("pune", 18.5f, 73.8f), new City("mumbai", 19.4f, 72.5f), new City("cochin", 9.9f, 76.2f), new City("shimla", 31.1f, 77.1f), new City("puducherry", 11.9f, 79.8f) };
+
+            Console.WriteLine("Please enter any of these two city codes");
+            Console.WriteLine("Pune - 0");
+            Console.WriteLine("Mumbai - 1");
+            Console.WriteLine("Cochin - 2");
+            Console.WriteLine("Shimla - 3");
+            Console.WriteLine("Puducherry - 4");
+
+            int num1 = Int32.Parse(Console.ReadLine());
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            double deg2Rad = (3.1415f / 180);
+
+            double rad1Lat = c[num1].lat * deg2Rad;
+            double rad1Lon = c[num1].lon * deg2Rad;
+
+            double rad2Lat = c[num2].lat * deg2Rad;
+            double rad2Lon = c[num2].lon * deg2Rad;
+
+            double x1 = CartesianCooX(rad1Lat, rad1Lon);
+            double y1 = CartesianCooY(rad1Lat, rad1Lon);
+            double z1 = CartesianCooZ(rad1Lat, rad1Lon);
+
+            double x2 = CartesianCooX(rad2Lat, rad2Lon);
+            double y2 = CartesianCooY(rad2Lat, rad2Lon);
+            double z2 = CartesianCooZ(rad2Lat, rad2Lon);
+
+            double fin = Distance(x1, y1, z1, x2, y2, z2);
+
+            //double distanceBetweenTwoCities = GetFinDist(fin);
+            Console.WriteLine("The distance between those two cities is " + fin);
+            Console.Read();
+        }
+
+        private static double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double temp1 = Math.Pow((x2 - x1), 2);
+            double temp2 = Math.Pow((y2 - y1), 2);
+            double temp3 = Math.Pow((z2 - z1), 2);
+            double result = Math.Sqrt(temp1 + temp2 + temp3);
+
+            double EarthRad = 6371;
+            double a = result / 2;
+
+            double r = 2 * (Math.Asin(a / EarthRad));
+            double finDist = r * EarthRad;
+            return finDist;
+        }
+
+        private static double CartesianCooX(double x1, double y1)
+        {
+            double EarthRad = 6371;
+
+            double ans = EarthRad * Math.Cos(x1) * Math.Cos(y1);
+
+            return ans;
+        }
+
+        private static double CartesianCooY(double x1, double y1)
+        {
+            double EarthRad = 6371;
+
+            double ans = EarthRad * Math.Cos(x1) * Math.Sin(y1);
+
+            return ans;
+        }
+
+        private static double CartesianCooZ(double x1, double y1)
+        {
+            double EarthRad = 6371;
+
+            double ans = EarthRad * Math.Sin(x1);
+
+            return ans;
+        }
+
+        /*private static double GetFinDist(double x)
+        {
+            double EarthRad = 6371;
+            double a = x / 2;
+
+            double result = 2 * (Math.Asin(a / EarthRad));
+            double finDist = result * EarthRad;
+
+            return finDist;
+        }*/
+    }
+
+
+}
